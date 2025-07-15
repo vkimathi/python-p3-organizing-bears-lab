b8b8f29aa07f8700b58fe590bcc58cbec52c4d57
Victor Kimathi <vkimathi97@gmail.com>
2025-07-16 00:19:16 +0300

    python-p3-organizing-bears-lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) values
+("Mr. Chocolate", 1, "M", "Red", "Cold", True),
+("Rowdy", 2, "M", "Orange", "Cold", True),
+("Tabitha", 3, "F", "Yellow", "Cold", True),
+("Sergeant Brown", 4, "M", "Green", "Cold", True),
+("Melissa", 5, "F", "Blue", "Warm", True),
+("Grinch", 6, "F", "Indigo", "Warm", False),
+("Wendy",  7, "F", "Violet", "Warm", False),
+(NULL, 8, 'F', 'Black', 'mysterious', False);
